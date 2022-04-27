e985c2d492f472c577416ed74118bb12b138d60c
佐々木　莉奈 <1072sasaki@gmail.com>
2022-04-27 17:10:05 +0900

    [add]編集機能を追加

--- todo-app/app/controllers/tasks_controller.rb
+++ todo-app/app/controllers/tasks_controller.rb
@@ -11,15 +11,29 @@ end
 def create
   @task = Task.new(task_params）
   if @task.save
-    redirect_to tasks_path　#セーブできたらindexページに行く
+    redirect_to tasks_path  #セーブできたらindexページに行く
   else
-    render 'new'　#できなかったらnewページのまま
+    render 'new'  #できなかったらnewページのまま
+  end
+end
+
+def edit
+  @task = Task.find(params[:id])
+end
+
+def update
+  @task = Task.find(params[:id])
+  if @task.update(task_params) 
+    redirect_to tasks_path　#updateできたらindexページに行く
+  else
+    render 'edit'　#できなかったらeditページのまま
   end
 end
 
 private
 def task_params
-  　　#モデル作成時に作ったやつ
+    #モデル作成時に作ったやつ
       params.require(:task).permit(:title, :body)
     end
   end
+
