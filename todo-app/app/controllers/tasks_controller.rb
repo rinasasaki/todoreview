8a3738aaf19e534c89c5ecd63856870a8bc5c9a2
佐々木　莉奈 <1072sasaki@gmail.com>
2022-04-27 15:38:54 +0900

    [add]新規作成機能を追加

--- todo-app/app/controllers/tasks_controller.rb
+++ todo-app/app/controllers/tasks_controller.rb
@@ -3,3 +3,23 @@ class TasksController < ApplicationController
     @tasks = Task.all
   end
 end
+
+def new
+  @task = Task.new
+end
+
+def create
+  @task = Task.new(task_params）
+  if @task.save
+    redirect_to tasks_path　#セーブできたらindexページに行く
+  else
+    render 'new'　#できなかったらnewページのまま
+  end
+end
+
+private
+def task_params
+  　　#モデル作成時に作ったやつ
+      params.require(:task).permit(:title, :body)
+    end
+  end
