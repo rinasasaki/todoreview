ba9fc5f24eadbbe16ef68092c4843f4ee11db5d7
佐々木　莉奈 <1072sasaki@gmail.com>
2022-04-27 17:55:11 +0900

    [fix]ビューの修正

--- todo-app/app/controllers/tasks_controller.rb
+++ todo-app/app/controllers/tasks_controller.rb
@@ -4,36 +4,3 @@ class TasksController < ApplicationController
   end
 end
 
-def new
-  @task = Task.new
-end
-
-def create
-  @task = Task.new(task_params）
-  if @task.save
-    redirect_to tasks_path  #セーブできたらindexページに行く
-  else
-    render 'new'  #できなかったらnewページのまま
-  end
-end
-
-def edit
-  @task = Task.find(params[:id])
-end
-
-def update
-  @task = Task.find(params[:id])
-  if @task.update(task_params) 
-    redirect_to tasks_path　#updateできたらindexページに行く
-  else
-    render 'edit'　#できなかったらeditページのまま
-  end
-end
-
-private
-def task_params
-    #モデル作成時に作ったやつ
-      params.require(:task).permit(:title, :body)
-    end
-  end
-
