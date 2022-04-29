ef44246fed64ba3bc97f1fbd5cd6caae81f0a179
佐々木　莉奈 <1072sasaki@gmail.com>
2022-04-29 16:30:20 +0900

    [fix]新規作成やり直し

--- todo-app/app/controllers/tasks_controller.rb
+++ todo-app/app/controllers/tasks_controller.rb
@@ -2,4 +2,24 @@ class TasksController < ApplicationController
   def index
     @tasks = Task.all
   end
+
+
+def new
+  @task = Task.new
+end
+
+def create
+  @task = Task.new(task_params)
+  if @task.save
+    redirect_to task_path(@task)
+  else
+    render :new
+  end
+end
+
+private
+  def task_params
+　　#モデル作成時に作ったやつ
+    params.require(:task).permit(:title, :body)
+  end
 end
