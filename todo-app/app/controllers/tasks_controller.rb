8a6811b69aedb34e592f2ce7b25129efb26c0276
佐々木　莉奈 <1072sasaki@gmail.com>
2022-04-30 23:32:32 +0900

    [add]コントローラーに編集機能を追加

--- todo-app/app/controllers/tasks_controller.rb
+++ todo-app/app/controllers/tasks_controller.rb
@@ -17,6 +17,19 @@ def create
   end
 end
 
+def edit
+  @task = Task.find(params[:id])
+end
+
+def update
+  @task = Task.find(params[:id])
+  if @task.update(task_params) 
+    redirect_to tasks_path
+  else
+    render 'edit'
+  end
+end
+
 private
   def task_params
     params.require(:task).permit(:title, :body)
